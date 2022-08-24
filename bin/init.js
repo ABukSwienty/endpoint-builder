1b0d0bb9bbe5a85af61cbd7574670c64edda73fd
ABukSwienty <alexanderbukswienty@gmail.com>
2022-08-24 13:48:07 +0200

    fix ts cmd

--- bin/init.js
+++ bin/init.js
@@ -1,10 +1,12 @@
 #! /usr/bin/env node
 var main = require("../lib/index");
 const execSync = require("child_process").execSync;
+const path = require("path");
 
 async function init() {
   await main.default();
-  execSync("tsc -p tsconfig.build.json");
+  const root = path.resolve("..", __dirname);
+  execSync(`cd ${root} && tsc -p tsconfig.build.json`);
 }
 
 init();
